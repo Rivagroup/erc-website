39336f19bd35990c127a2d293e7bc98a450083cd
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-02 14:06:19 +0200

    added controller, views from bc, edited overview

---
+++ application/views/kindergeburtstag/basic.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="card bg-info text-white">
+        <div class="card-header">
+            <h4 class="card-title"><small><i class="fas fa-birthday-cake"></i>  Kindergeburtstag</small> Basic</h4>
+        </div>
+        <div class="card-body">
+            <div class="card-text">
+                <p>für 5 Kinder (ab 4 Jahre) + 1 Begleitperson</p>            
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-info"><small><i class="fas fa-check"></i> Dauer 1,5 - 2 Stunden</small></li>
+                    <li class="list-group-item list-group-item-info"><small><i class="fas fa-check"></i> 2 Runden Bowling inkl. Leihschuhe</small></li>
+                    <li class="list-group-item list-group-item-info"><small><i class="fas fa-check"></i> 5 große Softdrinks</small></li>
+                    <li class="list-group-item list-group-item-info"><small><i class="fas fa-check"></i> bunter Knabber- & Süßigkeitenkorb für alle</small></li>
+                    <li class="list-group-item list-group-item-info"><small><i class="fas fa-check"></i> jeder weitere Party-Gast <b>9,50€</b></small></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card-footer">
+            <p class="display-3 text-center"><b>49 €</b></p>
+            <a class="btn btn-lg btn-block btn-primary disabled" href="#"><i class="far fa-calendar-plus"></i> jetzt Buchen</a>
+        </div>
+    </div>
+    <p>
+        <small class="text-muted">Zzgl. 3,00 € für jede weitere Begleitperson bei allen Kindergeburtstagspaketen</small>
+    </p>
+    <div class="mx-auto bg-danger text-warning container-fluid rounded">
+        Online Buchung gerade nicht möglich. Sie können Ihren Termin gern telefonisch unter
+        <a class="badge badge-primary" href="tel:+4937123627835">
+            <i class="fas fa-phone"></i> 0371 236 278 35
+        </a> vereinbaren.
+    </div>
+</div>
---
+++ application/views/kindergeburtstag/xxl.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="card bg-success text-white">
+        <div class="card-header">
+            <h4 class="card-title"><small><i class="fas fa-birthday-cake"></i> Kindergeburtstag</small> XXL</h4>
+        </div>
+        <div class="card-body">
+            <div class="card-text">
+                <p>für 5 Kinder (ab 4 Jahre) + 1 Begleitperson</p>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> Dauer 2,5 - 3 Stunden</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> 2 Runden Bowling inkl. Leihschuhe</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> 5 große Softdrinks</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> bunter Knabber- & Süßigkeitenkorb für alle</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> 
+                            Party - Food - Platte<br>
+                            <i><small>
+                                    bestehend aus frischen Gemüse-Sticks mit Dip, Steakhouse Pommes, würzigen Kartoffelspalten, Chicken-Nuggets, Chicken-Stripes und Mozarella-Sticks
+                                </small></i>
+                        </small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> ein tolles Gruppenfoto der Geburtstagsgesellschaft für jeden Teilnehmer</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> Auswertung der Spiele</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> eine Urkunde und einen kleinen Eisbecher mit 2 Kugeln für den Gewinnen</small></li>
+                    <li class="list-group-item list-group-item-success"><small><i class="fas fa-check"></i> jeder weitere Party-Gast <b>17,50€</b></small></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card-footer">
+            <p class="display-3 text-center"><b>89 €</b></p>
+            <a class="btn btn-lg btn-block btn-primary " href="#"><i class="far fa-calendar-plus"></i> jetzt Buchen</a>
+        </div>
+    </div>
+    <p>
+        <small class="text-muted">Zzgl. 3,00 € für jede weitere Begleitperson bei allen Kindergeburtstagspaketen</small>
+    </p>
+    <div class="mx-auto bg-danger text-warning container-fluid rounded">
+        Online Buchung gerade nicht möglich. Sie können Ihren Termin gern telefonisch unter
+        <a class="badge badge-primary" href="tel:+4937123627835">
+            <i class="fas fa-phone"></i> 0371 236 278 35
+        </a> vereinbaren.
+    </div>
+</div>
