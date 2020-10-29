9ff2720e2fa06b4b4a8c8d3a8a5d9c5f1fc25738
Oliver Oehme <oliver.oehme@gmail.com>
2020-10-29 11:48:53 +0100

    china virus update

--- application/views/template.php
+++ application/views/template.php
@@ -14,13 +14,12 @@
     <meta name="co-author" content="<?= $co_author ?>" />
     <meta name="pragma" content="<?= $cache ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/erc.bootstrap.min.css'); ?>">
-    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.15.1/css/all.css" crossorigin="anonymous">
     <link href="<?= base_url('assets/favicon.png'); ?>" rel="icon" type="image/png" />
 	<?= $meta_postal ?>
     <?= $meta_og ?>
     <?= $meta_itemprop ?>
     <?= $meta_links ?>
-    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link href="<?= base_url('assets/favicon.png'); ?>" rel="icon" type="image/png" />
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
